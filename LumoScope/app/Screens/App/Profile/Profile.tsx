9e1c9ec1613c8cb927aba8a8302e4ce1dccbd26b
Oloye Paul <66376866+Drizzy-hub@users.noreply.github.com>
2024-04-21 19:15:16 +0100

    Update Profile.tsx

--- LumoScope/app/Screens/App/Profile/Profile.tsx
+++ LumoScope/app/Screens/App/Profile/Profile.tsx
@@ -1,17 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
-import { Container } from '../../../../components'
+import { Button, Container, FormInput, Header, Text } from '../../../../components'
+import colors from '../../../../constants/Colors'
 
 const Profile = () => {
   return (
-    <View>
-      <Container>
+    <Container>
+      <Header title='Profile' />
+      <View style={{ paddingHorizontal: 24, marginTop: 30 }}>
+        <View>
+          <FormInput
+            // value={userData?.name}
+            style={styles.textBox}
+            placeholder="Name"
+          />
+          <FormInput
+            // value={user?.email}
+            placeholder={"Email"}
+            style={styles.textBox}
+          />
+          <FormInput
+            // value={userData?.phone}
+            placeholder={"Phone Number"}
+            style={styles.textBox}
+          />
+          <FormInput
+            // value={userData?.phone}
+            placeholder={"Location"}
+            style={styles.textBox}
+          />
 
-      </Container>
-    </View>
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Text style={{ color: colors.primaryBlue }}>Log out</Text>
+          <Text style={{ color: colors.red, marginTop: 10 }}>
+            Delete Account
+          </Text>
+        </View>
+        <View style={{ marginTop: 30 }}>
+          <Button style={{ borderRadius: 15 }} onPress={() => { }} text={"Update"} />
+        </View>
+      </View>
+    </Container>
+
   )
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textBox: {
+    marginBottom: 10
+  }
+})
