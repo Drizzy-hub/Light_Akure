bbeb9c930e9b004449b8f944a28f122b5c4170e1
Drizzy <66376866+Drizzy-hub@users.noreply.github.com>
2024-06-20 13:23:14 +0100

    chore: making production fixes

--- LumoScope/services/api.ts
+++ LumoScope/services/api.ts
@@ -1,21 +1,21 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.0.102/lumoscope/",
-    prepareHeaders: (headers) => {
-      // headers.set("Content-Type", "multipart/form-data");
-      return headers;
-    },
-  }),
-  endpoints: () => ({}),
-  tagTypes: ["GetLocation"],
+	baseQuery: fetchBaseQuery({
+		baseUrl: process.env.EXPO_PUBLIC_API_URL,
+		prepareHeaders: (headers) => {
+			// headers.set("Content-Type", "multipart/form-data");
+			return headers;
+		},
+	}),
+	endpoints: () => ({}),
+	tagTypes: ['GetLocation'],
 });
 export const {
-  reducer: apiReducer,
-  reducerPath: apiReducerPath,
-  middleware: apiMiddleware,
-  enhanceEndpoints,
-  injectEndpoints,
-  endpoints,
+	reducer: apiReducer,
+	reducerPath: apiReducerPath,
+	middleware: apiMiddleware,
+	enhanceEndpoints,
+	injectEndpoints,
+	endpoints,
 } = api;
--- LumoScope/services/auth.ts
+++ LumoScope/services/auth.ts
@@ -1,149 +1,140 @@
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { ImageRequireSource } from "react-native/types";
-import { injectEndpoints } from "./api";
+import { ImageRequireSource } from 'react-native/types';
+import { injectEndpoints } from './api';
 
 interface APIDataResponse<D> {
-  success: boolean;
-  data: D;
+	success: boolean;
+	data: D;
 }
 export interface APILoginResponse {
-  message: string;
-  success: boolean;
-  token: string;
-  user: {
-    name: string;
-    phone: string;
-    email: string;
-  };
+	message: string;
+	success: boolean;
+	token: string;
+	user: {
+		name: string;
+		phone: string;
+		email: string;
+	};
 }
 interface SignupModel {
-  Phone: string;
-  Email: string;
-  Name: string;
+	Phone: string;
+	Email: string;
+	Name: string;
 }
 interface LoginModel {
-  phone: string;
+	phone: string;
 }
 interface Location {
-  success: boolean;
-  data: LocationData[];
+	success: boolean;
+	data: LocationData[];
 }
 interface LocationData {
-  id: string;
-  location: string;
+	id: string;
+	location: string;
 }
 interface Timeline {
-  success: boolean;
-  data: TimelineData[];
+	success: boolean;
+	data: TimelineData[];
 }
 export interface TimelineData {
-  id: string;
-  title: string;
-  description: string;
-  image_path: string;
-  time_created: string;
+	id: string;
+	title: string;
+	description: string;
+	image_path: string;
+	time_created: string;
 }
 export interface LightStatus {
-  success: boolean;
-  data: LightData[];
+	success: boolean;
+	data: LightData[];
 }
 
 interface LightData {
-  id: string;
-  location: string;
-  is_faulty: boolean;
-  is_light: boolean;
-  light_start: string;
-  light_end: string;
-  notification_sent?: string;
-  duration: string;
-  availability: string;
+	id: string;
+	location: string;
+	is_faulty: boolean;
+	is_light: boolean;
+	light_start: string;
+	light_end: string;
+	notification_sent?: string;
+	duration: string;
+	availability: string;
 }
 interface Barchart {
-  success: boolean;
-  data: BarData[];
+	success: boolean;
+	data: BarData[];
 }
 
 export interface BarData {
-  label: string;
-  value: number;
+	label: string;
+	value: number;
 }
 
 const authenticationEndpoints = injectEndpoints({
-  endpoints: (builder) => ({
-    signup: builder.mutation<APIDataResponse<SignupModel>, SignupModel>({
-      query: (body) => ({
-        body: JSON.stringify(body),
-        method: "POST",
-        url: `/signup.php?${body}`,
-      }),
-    }),
-    login: builder.mutation<APILoginResponse, LoginModel>({
-      query: (body) => ({
-        body: JSON.stringify(body),
-        method: "POST",
-        url: `/login.php?${body}`,
-      }),
-    }),
-    getLocation: builder.query<Location, void>({
-      query: () => ({
-        url: `/get_location.php`,
-      }),
-    }),
+	endpoints: (builder) => ({
+		signup: builder.mutation<APIDataResponse<SignupModel>, SignupModel>({
+			query: (body) => ({
+				body: JSON.stringify(body),
+				method: 'POST',
+				url: `/signup.php?${body}`,
+			}),
+		}),
+		login: builder.mutation<APILoginResponse, LoginModel>({
+			query: (body) => ({
+				body: JSON.stringify(body),
+				method: 'POST',
+				url: `/login.php?${body}`,
+			}),
+		}),
+		getLocation: builder.query<Location, void>({
+			query: () => ({
+				url: `/get_location.php`,
+			}),
+		}),
 
-    getTimeline: builder.query<Timeline, void>({
-      query: () => ({
-        url: `/get_timeline.php`,
-      }),
-    }),
-    getTimelineById: builder.query<
-      APIDataResponse<TimelineData>,
-      { id: string }
-    >({
-      query: ({ id }) => ({
-        url: `/get_timelineby_id.php?id=${id}/`,
-      }),
-    }),
-    getLightStatusByID: builder.query<
-      APIDataResponse<LightData>,
-      { id: string }
-    >({
-      query: ({ id }) => ({
-        url: `/get_light_status_id.php?id=${id}/`,
-      }),
-    }),
-    getLight: builder.query<LightStatus, void>({
-      query: () => ({
-        url: `/get_light_status.php`,
-      }),
-    }),
-    getChart: builder.query<Barchart, { id: string }>({
-      query: ({ id }) => ({
-        url: `/getChart.php?id=${id}/`,
-      }),
-    }),
-  }),
+		getTimeline: builder.query<Timeline, void>({
+			query: () => ({
+				url: `/get_timeline.php`,
+			}),
+		}),
+		getTimelineById: builder.query<
+			APIDataResponse<TimelineData>,
+			{ id: string }
+		>({
+			query: ({ id }) => ({
+				url: `/get_timelineby_id.php?id=${id}/`,
+			}),
+		}),
+		getLightStatusByID: builder.query<
+			APIDataResponse<LightData>,
+			{ id: string }
+		>({
+			query: ({ id }) => ({
+				url: `/get_light_status_id.php?id=${id}/`,
+			}),
+		}),
+		getLight: builder.query<LightStatus, void>({
+			query: () => ({
+				url: `/get_light_status.php`,
+			}),
+		}),
+		getChart: builder.query<Barchart, { id: string }>({
+			query: ({ id }) => ({
+				url: `/getChart.php?id=${id}/`,
+			}),
+		}),
+	}),
 
-  // editProfile: builder.mutation<void, EditProfileModel>({
-  //   invalidatesTags: ["GetUser"],
-  //   query: (body) => ({
-  //     body,
-  //     method: "POST",
-  //     url: "v1/auth/users/update-profile/",
-  //   }),
-  // }),
-
-  overrideExisting: true,
+	overrideExisting: true,
 });
 
 export const {
-  endpoints: authEndpoints,
-  useSignupMutation,
-  useLoginMutation,
-  useGetLocationQuery,
-  useGetTimelineQuery,
-  useGetTimelineByIdQuery,
-  useGetLightQuery,
-  useGetLightStatusByIDQuery,
-  useGetChartQuery,
+	endpoints: authEndpoints,
+	useSignupMutation,
+	useLoginMutation,
+	useGetLocationQuery,
+	useGetTimelineQuery,
+	useGetTimelineByIdQuery,
+	useGetLightQuery,
+	useGetLightStatusByIDQuery,
+	useGetChartQuery,
 } = authenticationEndpoints;
