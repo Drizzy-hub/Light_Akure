9e200cf0aec4260b158c42cfbf2b41e01b05652d
Drizzy <66376866+Drizzy-hub@users.noreply.github.com>
2024-08-04 22:39:38 +0100

    chore: ios fix

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useCallback } from 'react';
 import { AppNavigator } from './app/Navigation';
@@ -57,12 +56,3 @@ export default function App() {
 		</GestureHandlerRootView>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
